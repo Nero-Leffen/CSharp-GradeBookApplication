ddddd3d9c26cecff9edab6138bbed70308a6a876
Nero-Leffen <ipsit.ex0309@gmail.com>
2022-12-05 19:06:09 +0530

    Task 1 Completed

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+
+    }
+}
